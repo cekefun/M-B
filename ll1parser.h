c4299c23294281d9bda32b02ab7314adcc18cf37
cekefun <cekefun@gmail.com>
2016-11-04 17:29:41 +0100

    added documentation to ll1parser

--- ll1parser.h
+++ ll1parser.h
@@ -6,10 +6,32 @@
 class LL1Parser{
 	public:
 		LL1Parser();
+		/*
+		Standard constructor.
+		Sets grammar to a new standard grammar, error to "ERROR" and EOS to "EOS"
+		*/
 		LL1Parser(Grammar*);
+		/*
+		Constructor with grammar as input.
+		grammar is set to the input.
+		error is set to "ERROR" if that is not taken. 
+		If it is taken, it will use "ERRORi" with i the lowest integer <= 1 so "ERRORi" is not in the grammar
+		EOS is set to "EOS" if that is not taken.
+		If it is taken, it will use "EOS" with i the lowest integer <= 1 so "EOS" is not in the grammar
+		*/
 		bool setGrammar(Grammar*);
+		/*
+		Sets grammar to input and recalculates ERROR and EOS. (see LL1Parser(Grammar*))
+		*/
 		bool Print(std::ostream&);
+		/*
+		Prints the ll(1) table to the ostream
+		*/
 		bool Parse(word);
+		/*
+		Checks wether the word is part of the grammar or not.
+		*/
+
 	private:
 		bool CreateTable();
 		Grammar *grammar;
